99ea9328985f3cbfa0059fc2ba9219dd6bd2b41a
Reenjay <reenjie17@gmail.com>
2022-09-06 09:02:15 +0800

    added the framework and routings

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react'
+
+import Routing from './routes/web';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ChakraProvider>
-      <TheRestOfYourApplication />
+        <Routing />
   </ChakraProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
