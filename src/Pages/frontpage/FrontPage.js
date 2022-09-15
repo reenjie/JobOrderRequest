18ff0559f7e7014fc3992502af35fe67ee17995e
aseyte2 <dennisfalcasantosnatividad@gmail.com>
2022-09-15 09:49:03 +0800

    Starting for frontpage remodel

--- src/Pages/frontpage/FrontPage.js
+++ src/Pages/frontpage/FrontPage.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { Component, useState, useEffect, Fragment } from "react";
 import * as _ from "@chakra-ui/react";
 import logo from "../../images/zcmc_logo.png";
 import Axios from "axios";
@@ -12,73 +12,115 @@ import {
   GridItem,
   Box,
   Flex,
+  Textarea,
   Heading,
   Checkbox,
   Stack,
   Select,
   Button,
   Spacer,
+  Divider,
   TableContainer,
   Table,
   Thead,
   Tbody,
   Tfoot,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
   Tr,
+  Input,
   Th,
   Td,
   TableCaption,
   Container,
   Image,
   Icon,
+  FormControl,
+  FormLabel,
+  Text,
+  FormErrorMessage,
+  FormHelperText,
+  Center,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-
-const Tablef = (props) => {
-  return props.data == "" ? (
-    "Choose Type of work"
-  ) : (
-    <Tbody>
-      <Tr>
-        <Td>{props.data.jobType}</Td>
-        <Td>{props.data.serviceDepartment}</Td>
-        <Td>{props.data.serviceType}</Td>
-        <Td>
-          <Button
-            colorScheme="red"
-            variant="ghost"
-            size={"sm"}
-            onClick={() => {
-              props.deleteHundler(props.data);
-            }}
-          >
-            <DeleteIcon />
-          </Button>
-        </Td>
-      </Tr>
-    </Tbody>
-  );
-};
-
-class RequestModel {
-  constructor(jobType, serviceDepartment, serviceType) {
-    this.jobType = jobType;
-    this.serviceDepartment = serviceDepartment;
-    this.serviceType = serviceType;
-  }
-}
+import Edit_Modal from "../../components/layouts/edit_modal";
 
 const FrontPage = () => {
+  ////Fetch Data save to this local var
   const [typeofJob, setTypeofJob] = useState([]); /// List of Job Type eg. Repaire or Installation
   const [optionService, setOptionService] = useState([]); ///ex. List of Department
   const [optionOffer, setOptionOffer] = useState([]); //ex. List of offer based on selected Department
-  const [tableData, setTableData] = useState([]); /// For table,
-  const [addWork, setWork] = useState("");
-  const [addRequest, setRequest] = useState([]);
+
+  const [defRequestData, setDefRequestData] = useState([]); ///Local Variable for default table ROW for NON OTHERS or NON SERIAL
+  const [isOthersRequestData, setOthersRequestData] = useState([]); //Local Variable for Others request
+  const [hasNumRequestData, sethasNumRequestData] = useState([]); //Local Variable for Request has Serial and Model Number
+
   const [selectedTOJ, setselectedTOJ] = useState(); ///Selected Type of job, eg. Repaire
   const [selectedDepartment, setselectedDepartment] = useState(); ///Selected Department eg. Civil
   const [selectedSOffer, setselectedSOffer] = useState(); /// Selected offer
-  const [isShown, setShow] = useState(false);
 
+  const [isShown, setShow] = useState(false); //if checkBox selected || for TextBox view
+  const [isEleMech, setEleMech] = useState(); ////
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [data, setData] = useState([]);
+  const [checkedBox, setCheckedBox] = React.useState(false); //// CheckBOx
+  const [tableData, setTableData] = useState([]); /// For table,
+  const [addWork, setWork] = useState("");
+
+  // const TypeOfWork = ["New Installation/Fabrication", "Repair/Renovation"];
+
+  const TableComposition = (props) => {};
+
+  const Tablef = (props) => {
+    return props.data == "" ? (
+      "Choose Type of work"
+    ) : (
+      <Tbody>
+        <Tr>
+          <Td>
+            <Box>{props.data.jobType}</Box>
+            {/* <Box>
+              <Edit_Modal
+                btnTitle="ADD"
+                title="Add Services  "
+                mbody={Tex}
+              />
+              <Box as="span" color="gray.600" fontSize="sm">
+                Details
+              </Box>
+            </Box> */}
+          </Td>
+          <Td>{props.data.serviceDepartment}</Td>
+          <Td>{props.data.serviceType}</Td>
+          <Td>
+            <Button
+              colorScheme="red"
+              variant="ghost"
+              size={"sm"}
+              onClick={() => {
+                props.deleteHundler(props.data);
+              }}
+            >
+              <DeleteIcon />
+            </Button>
+          </Td>
+        </Tr>
+      </Tbody>
+    );
+  };
+
+  class DefRequestModel {
+    constructor(jobType, serviceDepartment, serviceType) {
+      this.jobType = jobType;
+      this.serviceDepartment = serviceDepartment;
+      this.serviceType = serviceType;
+    }
+  }
+
+  console.log(new DefRequestModel());
   useEffect(() => {
     Axios.post("http://localhost/JOBREQUEST/api/admin/getservices.php").then(
       (req) => {
@@ -116,6 +158,21 @@ const FrontPage = () => {
       </Button>
     );
   };
+
+  ////CheckBox
+  function CheckBoxHundler() {
+    return (
+      <Checkbox
+        isChecked={checkedBox}
+        onChange={(e) => {
+          setCheckedBox(e.target.checked);
+        }}
+      >
+        Others
+      </Checkbox>
+    );
+  }
+
   const updateTable = () => {
     console.log("toinks", selectedDepartment);
     if (
@@ -129,10 +186,11 @@ const FrontPage = () => {
       //if one the selection is empty
       console.log("some of the selection is null");
     } else {
-      setRequest((oldArray) => [
+      setDefRequestData((oldArray) => [
         ...oldArray,
-        new RequestModel(
+        new DefRequestModel(
           typeofJob.filter((e) => e.PK_workTypeID == selectedTOJ)[0].label,
+          //selectedTOJ,
           optionService.filter(
             (e) => e.PK_servicesID == selectedDepartment
           )[0].name,
@@ -142,9 +200,31 @@ const FrontPage = () => {
     }
   };
 
-  const Test = () => {
+  //hundle text area.. OTHERS
+  function TextBox() {
+    let [value, setValue] = React.useState("");
+
+    let handleInputChange = (e) => {
+      let inputValue = e.target.value;
+      setValue(inputValue);
+    };
+    return (
+      <>
+        {/* <Text mb="8px">Value: {value}</Text> */}
+        <Textarea
+          value={value}
+          onChange={handleInputChange}
+          placeholder="Complaints/Observations/Descriptions: (Kindly specify in details if possible)"
+          size="sm"
+          maxHeight={"200"}
+        />
+      </>
+    );
+  }
+  const SelectedDepartment = () => {
     return optionOffer.length <= 0 ? null : (
       <Select
+        isDisabled={checkedBox}
         variant="filled"
         placeholder="Select type of service"
         value={selectedSOffer}
@@ -163,9 +243,47 @@ const FrontPage = () => {
     );
   };
 
+  ////Image Upload
+  const UploadAndDisplayImage = () => {
+    return (
+      <Flex flexDirection={"row"}>
+        {selectedImage && (
+          <div>
+            <img
+              alt="not fount"
+              width={"250px"}
+              src={URL.createObjectURL(selectedImage)}
+            />
+            <br />
+            <button onClick={() => setSelectedImage(null)}>Remove</button>
+          </div>
+        )}
+        <br />
+        <br />
+        <input
+          type="file"
+          name="myImage"
+          onChange={(event) => {
+            console.log(event.target.files[0]);
+            setSelectedImage(event.target.files[0]);
+          }}
+        />
+      </Flex>
+    );
+  };
+
+  const FormDetails = (props) => {
+    return (
+      <FormControl isDisabled={!isEleMech}>
+        <FormLabel>{props.name}</FormLabel>
+        <Input placeholder={props.placeholder} />
+      </FormControl>
+    );
+  };
+
   const deleteHundler = (requestModel) => {
-    const result = addRequest.filter((e) => requestModel != e);
-    setRequest(result);
+    const result = defRequestData.filter((e) => requestModel != e);
+    setDefRequestData(result);
   };
 
   const selectServiceHundlerTB = (arg) => {
@@ -188,152 +306,190 @@ const FrontPage = () => {
     return isShown == false ? null : {};
   };
   return (
-    <Stack
-      justifyContent="center"
-      alignItems="center"
-      width="100wh"
-      height="100vh"
-      backgroundColor={"rgb(221, 222, 223)"}
-      flexDirection={"column"}
-      gap="5"
-    >
-      <Image boxSize={150} src={logo} alt="Dan Abramov" />
-      <Heading>Job Request Form</Heading>
+    <React.Fragment>
       <Stack
-        justifyContent={"center"}
+        justifyContent="center"
         alignItems="center"
+        width="100wh"
+        height="100vh"
+        backgroundColor={"rgb(221, 222, 223)"}
         flexDirection={"column"}
-        p="5"
-        bg="white"
-        borderRadius="10"
-        boxShadow="2xl"
-        width={"100vh"}
+        gap="5"
       >
-        <Heading as="h1" size="sm">
-          Select Type of Work{" "}
-        </Heading>
-        <ShowAlert />
-        <Stack direction={"row"}>
-          <Select
-            variant="filled"
-            placeholder="Select type of service"
-            value={selectedTOJ}
-            onChange={(e) => {
-              {
-                setselectedTOJ(e.target.value);
-              }
-            }}
-          >
-            {typeofJob.map((e, index) => {
-              return (
-                <option value={e.PK_workTypeID} key={index}>
-                  {e.label}
-                </option>
-              );
-            })}
-          </Select>
-          <Select
-            variant="filled"
-            placeholder="Select option"
-            value={selectedDepartment}
-            onChange={(e) => {
-              selectServiceHundlerTB(e.target.value);
-              console.log("The selected is ", e.target.value);
-              setselectedDepartment(e.target.value);
-              console.log(
-                "value :",
-                e.target.value == "" ? "nothing" : e.target.value
-              );
-            }}
-          >
-            {optionService.map((e, index) => {
-              return (
-                <option value={e.PK_servicesID} key={e.PK_servicesID}>
-                  {e.name}
-                </option>
-              );
-            })}
-            {/*  */}
-          </Select>
-          <Test />
-        </Stack>
-        <Container maxWidth={"container.xxl"}>
-          {" "}
-          {selectedDepartment && selectedSOffer && selectedTOJ ? (
-            <Button
-              float={"right"}
-              colorScheme="teal"
-              variant="solid"
-              onClick={() => {
-                updateTable();
-                console.log(
-                  "Table: ",
-                  selectedTOJ,
-                  selectedDepartment,
-                  selectedSOffer
-                );
-              }}
-            >
-              Add
-            </Button>
-          ) : (
-            <Button
-              disabled
-              colorScheme="teal"
-              variant="solid"
-              float={"right"}
-              onClick={() => {
-                updateTable();
-                console.log(
-                  "Table: ",
-                  selectedTOJ,
-                  selectedDepartment,
-                  selectedSOffer
-                );
-              }}
-            >
-              Add
-            </Button>
-          )}
-        </Container>
-      </Stack>
-      <Stack
-        justifyContent={"center"}
-        alignItems="center"
-        flexDirection={"column"}
-        p="5"
-        bg="white"
-        borderRadius="10"
-        boxShadow="2xl"
-        width={"100vh"}
-      >
-        <TableContainer width={"100vh"} maxH="40vh" overflowY="auto">
-          <Table size="sm">
-            <Thead>
-              <Tr>
-                <Th>Job to be done</Th>
-                <Th>Type of service</Th>
-                <Th>Works</Th>
-              </Tr>
-            </Thead>
-            {addRequest.map((e, index) => {
-              return (
-                <Tablef data={e} key={index} deleteHundler={deleteHundler} />
-              );
-            })}
-          </Table>
-        </TableContainer>
-        <Container maxWidth={"container.xxl"}>
-          {addRequest.length > 0 ? (
-            <Link to="../Admin/Dashboard">
-              <Button colorScheme={"teal"} float={"right"}>
-                Send Request
+        <Image boxSize={150} src={logo} alt="Dan Abramov" />
+        <Heading>Job Request Form</Heading>
+        <Stack
+          justifyContent={"center"}
+          alignItems="center"
+          flexDirection={"column"}
+          p="5"
+          bg="white"
+          borderRadius="10"
+          boxShadow="2xl"
+          width={"100vh"}
+        >
+          <Heading as="h1" size="sm">
+            Select Type of Work{" "}
+          </Heading>
+          <ShowAlert />
+          <Flex direction={"row"} gap={6}>
+            <Box w="100%">
+              {/* <Select
+                variant="filled"
+                placeholder="Select type of service"
+                value={selectedTOJ}
+                onChange={(e) => {
+                  {
+                    setselectedTOJ(e.target.value);
+                  }
+                }}
+              >
+                {TypeOfWork.map((e, index) => {
+                  return (
+                    <option value={e} key={index}>
+                      {e}
+                    </option>
+                  );
+                })}
+              </Select> */}
+              <Select
+                variant="filled"
+                placeholder="Select type of service"
+                value={selectedTOJ}
+                onChange={(e) => {
+                  {
+                    setselectedTOJ(e.target.value);
+                  }
+                }}
+              >
+                {typeofJob.map((e, index) => {
+                  return (
+                    <option value={e.PK_workTypeID} key={index}>
+                      {e.label}
+                    </option>
+                  );
+                })}
+              </Select>
+            </Box>
+            <Box w="100%">
+              <Select
+                isDisabled={checkedBox}
+                variant="filled"
+                placeholder="Select option"
+                value={selectedDepartment}
+                onChange={(e) => {
+                  selectServiceHundlerTB(e.target.value);
+                  console.log("The selected is ", e.target.value);
+                  setselectedDepartment(e.target.value);
+                  setEleMech(e.target.value == 2 || e.target.value == 3);
+                  setselectedSOffer("");
+                  console.log(
+                    "On changed Data  ",
+                    selectedTOJ,
+                    selectedDepartment,
+                    selectedSOffer
+                  );
+                }}
+              >
+                {optionService.map((e, index) => {
+                  return (
+                    <option value={e.PK_servicesID} key={e.PK_servicesID}>
+                      {e.name}
+                    </option>
+                  );
+                })}
+                {/*  */}
+              </Select>
+              <Center>
+                <CheckBoxHundler />
+              </Center>
+            </Box>
+            <Box w="100%">
+              <SelectedDepartment />
+            </Box>
+          </Flex>
+
+          {/* text input */}
+          {checkedBox === false ? null : <TextBox />}
+          <Divider borderColor={"red"} w="80vh" p={2} />
+          <Stack direction={"row"} pt={2}>
+            <FormDetails placeholder={"123456"} name={"Serial No"} />
+            <FormDetails placeholder={"abc123"} name={"Model No"} />
+          </Stack>
+          <Container maxWidth={"container.xxl"}>
+            {" "}
+            {selectedDepartment && selectedSOffer && selectedTOJ ? (
+              <Button
+                float={"right"}
+                colorScheme="teal"
+                variant="solid"
+                onClick={() => {
+                  updateTable();
+                }}
+              >
+                Add
               </Button>
-            </Link>
-          ) : null}
-        </Container>
+            ) : (
+              <Button
+                disabled
+                colorScheme="teal"
+                variant="solid"
+                float={"right"}
+                onClick={() => {
+                  updateTable();
+                  console.log(
+                    "Table: ",
+                    selectedTOJ,
+                    selectedDepartment,
+                    selectedSOffer
+                  );
+                }}
+              >
+                Add
+              </Button>
+            )}
+          </Container>
+          <UploadAndDisplayImage />
+        </Stack>
+        <Stack
+          justifyContent={"center"}
+          alignItems="center"
+          flexDirection={"column"}
+          p="5"
+          bg="white"
+          borderRadius="10"
+          boxShadow="2xl"
+          width={"100vh"}
+        >
+          <TableContainer width={"100vh"} maxH="40vh" overflowY="auto">
+            <Table size="sm" variant="simple" colorScheme="teal">
+              <Thead>
+                <Tr>
+                  <Th>Job to be done</Th>
+                  <Th>Type of service</Th>
+                  <Th>Works</Th>
+                </Tr>
+              </Thead>
+              {defRequestData.map((e, index) => {
+                //Render Datas to be submit
+                return (
+                  <Tablef data={e} key={index} deleteHundler={deleteHundler} />
+                );
+              })}
+            </Table>
+          </TableContainer>
+          <Container maxWidth={"container.xxl"}>
+            {defRequestData.length > 0 ? (
+              <Link to="../Admin/Dashboard">
+                <Button colorScheme={"teal"} float={"right"}>
+                  Send Request
+                </Button>
+              </Link>
+            ) : null}
+          </Container>
+        </Stack>
       </Stack>
-    </Stack>
+    </React.Fragment>
   );
 };
 
